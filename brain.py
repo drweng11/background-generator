23d2cbcf7f67e6620f8e5e56c2acabc75e2f2df8
drweng11 <61410579+drweng11@users.noreply.github.com>
2020-05-26 08:58:50 +0800

    add print

--- brain.py
+++ brain.py
@@ -4,9 +4,13 @@ execution_path = os.getcwd()
 
 prediction = ImagePrediction()
 prediction.setModelTypeAsSqueezeNet()
-prediction.setModelPath(os.path.join(execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"))
+prediction.setModelPath(os.path.join(
+    execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"))
 prediction.loadModel()
 
-predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "house.jpg"), result_count=5 )
+predictions, probabilities = prediction.predictImage(
+    os.path.join(execution_path, "house.jpg"), result_count=5)
 for eachPrediction, eachProbability in zip(predictions, probabilities):
-    print(eachPrediction , " : " , eachProbability)
+    print(eachPrediction, " : ", eachProbability)
+
+print('hello')
